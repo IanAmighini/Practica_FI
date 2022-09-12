2739366d4db87d6a9f06a2ce4c858e56437e455c
IanAmighini <ianamighini@gmail.com>
2022-09-12 14:30:54 -0300

    commit 1 12/9

--- Clase5/Practica5.py
+++ Clase5/Practica5.py
@@ -3,19 +3,29 @@
 #1
 #Si es correcto, pero es impreciso. Se tendria que usar try ... except TypeError.
 
-#2
-
+#2 (modificar)
 def dividir(lista1, numero):
     lista2 = []
-    try:
-        for indice in lista1:
-            lista2.append(lista1[indice]/numero)
-        return lista2
-    except ZeroDivisionError:
-        "No se puede dividir por cero"
-    except TypeError:
-        "No es numero"
+    for num in lista1:
+        try:
+            lista2.append(lista1[num]/numero)
+        except ZeroDivisionError:
+            return "No se puede dividir por cero"
+        except TypeError:
+            return "No es numero"
+    return lista2
     
 print(dividir([2,4,6,8], 2))
 
 #3
+def positivo(lista, numero):
+    try:
+        if numero > 0:
+            lista.append(numero)
+            return lista
+        else:
+            return "El numero es negativo"
+    except TypeError:
+        return "No es numero"
+
+print(positivo([2,4,6,8], 'hola'))
--- Clase5/Teoria5.py
+++ Clase5/Teoria5.py
@@ -3,16 +3,29 @@
 #Desafio I: Descargá y ejecutá el script1_manejo_errores.py
 
 #Para pensar 🤔: ¿Qué tipo de error se obtiene al ejecutar el programa? ¿En dónde se encuentra el error? ¿Cómo te das cuenta?
+#tira un SintaxError en la linea 14. El propio error te dice line X (donde se encuentra el error)
 
 #Para pensar 🤔: ¿Qué nos dice el mensaje de excepción? ¿Qué es la excepción de nombre?
+#nos dice que el termino divisor no esta definido. La excepcion es omitir algo un una situacion.
 
 #Para pensar 🤔: ¿Qué nos dice el último mensaje de excepción? ¿Qué es la excepción de tipo?
+#Nos tira un error de TypeError, es decir que lo que ingresamos esta mal escrito. En este caso dice que no puede sumar un int con un str
 
 #Para pensar 🤔: ¿Qué tipo de excepción se maneja en el código anterior?
+#cualquier error lo va a omitir
 
 #Desafio II: Creá una función denominada mitades que tenga como argumento un número e imprima el resultado de la división de ese número por 2
+def mitades(num):
+    return num / 2
+print(mitades(0))
 
 #Para pensar 🤔: ¿Qué crees que ocurrirá cuando ingresas un 9 como parámetro? ¿Y con un 0?
+#nada, dividir 9 en 2 da 4.5 y dividir 0 por 2 da 0 
 
 #Desafio III: ¿Cómo mejorarías tu función para que sea capaz de manejar el error de la división por cero? Reescribí la función incorporando una declaración try-except
-
+def mitades(num, num2):
+    try: 
+        return num / num2
+    except ZeroDivisionError:
+        return 'No se puede dividir por cero'
+print(mitades(3,0))
